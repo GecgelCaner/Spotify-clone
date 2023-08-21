c36c7ac907e91cecc1816c70990b70bf816b058f
GecgelCaner <gecgelcaner.10@gmail.com>
2023-08-21 16:28:58 +0300

    navbar sidebar content buttonbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import BottomBar from "components/BottomBar";
+import Content from "components/Content";
+import SideBar from "components/SideBar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Content />
+        <SideBar />
+      </div>
+
+      <BottomBar />
+    </>
+  );
+}
+
+export default App;
